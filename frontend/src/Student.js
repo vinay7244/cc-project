8f486d39c0572c29ed1cc1375540fe9aa39ba764
vinay7244 <95561432+vinay7244@users.noreply.github.com>
2023-04-24 17:01:01 +0530

    Update Student.js

--- frontend/src/Student.js
+++ frontend/src/Student.js
@@ -24,7 +24,7 @@ function Student() {
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-3'>
-            <h2>DDE</h2>
+            <h2>DaaE</h2>
                 <Link to="/create" className='btn btn-success'>ADD +</Link>
                 <table className='table'>
                     <thead>
